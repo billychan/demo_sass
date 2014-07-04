0b800da15072d00c583bd241b1506d50d5ba2cd7
Billy <billyconnect@gmail.com>
2014-07-04 16:13:48 +0800

    basic html

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,49 @@
 class CandidatesController < ApplicationController
   def index
+    @job        = Job.get_job
+    @candidates = @job.candidates
+  end
+
+  # Simulate modle calls
+  class Job
+    def self.get_job
+      self.new
+    end
+
+    def other_jobs
+    end
+
+    def candidates
+      Candidates.all
+    end
+
+    def title
+      "X" * 20
+    end
+
+    def id
+      1
+    end
+
+    def status
+    end
+
+    def user_position
+      "Hiring Manager"
+    end
+
+    def other_jobs
+      Array.new 10, self.class.new
+    end
+  end
+
+  class Candidates
+    def self.all
+      Array.new 5, self.new
+    end
+
+    def job
+      Job.get_job
+    end
   end
 end
